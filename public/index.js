3cae817af17e790ed893de3891d985de07a489e2
Cristofer Fuentes <github@cristoferfb.com>
2020-06-08 21:00:44 -0400

    fix style issues

--- public/index.js
+++ public/index.js
@@ -1,9 +1,11 @@
+// Go to specific view (login, user, admin)
 function goTo (name) {
 	hideAll()
 	// reveal only the selected view
 	$("#"+name).css("display", "block")
 }
 
+// hide all views
 function hideAll () {
 	// styles of  login
 	$("body").css("display", "block")
@@ -15,55 +17,55 @@ function hideAll () {
 }
 
 function evaluateLogin () {
-	// Evaluate login
+	// TODO Evaluate login
 	goTo("user")
 }
 
-function createNode(productObj){
+// get a product node for insert in DOM
+function obtenerNodoProducto(product) {
+  var card = document.createElement("div");
+  card.classList.add("card");
+  card.classList.add("cardProducto");
 
-    var card = document.createElement("div");
-    card.classList.add("card");
-    card.classList.add("cardProducto");
+  var imagen = document.createElement("img");
+  imagen.src = product.img;
+  imagen.classList.add("imgProducto");
+  card.appendChild(imagen);
 
-    var imagen = document.createElement("img");
-    imagen.src = productObj.img;
-    imagen.classList.add("imgProducto");
-    card.appendChild(imagen);
-
-    var cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
+  var cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
     
-      var nombre = document.createElement("h5");
-      nombre.appendChild(document.createTextNode(productObj.nombre));
-      cardBody.appendChild(nombre);
+  var nombre = document.createElement("h5");
+  nombre.appendChild(document.createTextNode(product.nombre));
+  cardBody.appendChild(nombre);
 
-      var valor = document.createElement("p");
-      valor.appendChild(document.createTextNode(productObj.valor));
-      cardBody.appendChild(valor);
+  var valor = document.createElement("p");
+  valor.appendChild(document.createTextNode(product.valor));
+  cardBody.appendChild(valor);
 
-      var disponibilidad = document.createElement("p");
-      disponibilidad.appendChild(document.createTextNode("Disponibilidad"));
-      cardBody.appendChild(disponibilidad);
-      
-      var boton = document.createElement("a");
-      boton.classList.add("btn");
-      boton.classList.add("btnCompra");
+  var disponibilidad = document.createElement("p");
+  disponibilidad.appendChild(document.createTextNode("Disponibilidad"));
+  cardBody.appendChild(disponibilidad);
+  
+  var boton = document.createElement("a");
+  boton.classList.add("btn");
+  boton.classList.add("btnCompra");
 
-        var icon = document.createElement("i");
-        icon.classList.add("fas");
-        icon.classList.add("fa-cart-plus");
-        icon.classList.add("fa-lg");
-        icon.classList.add("iconoCarrito");
-        boton.appendChild(icon);
+  var icon = document.createElement("i");
+  icon.classList.add("fas");
+  icon.classList.add("fa-cart-plus");
+  icon.classList.add("fa-lg");
+  icon.classList.add("iconoCarrito");
+  boton.appendChild(icon);
 
-        var btnText = document.createElement("p");
-        btnText.classList.add("iconoCarrito");
-        btnText.appendChild(document.createTextNode("Agregar al carro"));
-        
-        boton.appendChild(btnText);
-      
-      cardBody.appendChild(boton);
-    card.appendChild(cardBody);
+  var btnText = document.createElement("p");
+  btnText.classList.add("iconoCarrito");
+  btnText.appendChild(document.createTextNode("Agregar al carro"));
+  
+  boton.appendChild(btnText);
+  
+  cardBody.appendChild(boton);
+  card.appendChild(cardBody);
 
-    return card;
+  return card;
 }
