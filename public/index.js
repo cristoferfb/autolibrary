d837d07ecd7e04991db98e05c0f63154a98ceee9
Cristofer Fuentes <github@cristoferfb.com>
2020-06-10 05:59:48 -0400

    Add feature product gallery

--- public/index.js
+++ public/index.js
@@ -2,70 +2,87 @@
 function goTo (name) {
 	hideAll()
 	// reveal only the selected view
-	$("#"+name).css("display", "block")
+	$('#'+name).css('display', 'block')
 }
 
 // hide all views
 function hideAll () {
 	// styles of  login
-	$("body").css("display", "block")
-	$("body").css("align-items", "left")
+	$('body').css('display', 'block')
+	$('body').css('align-items', 'left')
 	// hide all views
-	$('#login').css("display", "none")
-	$('#admin').css("display", "none")
-	$('#user').css("display", "none")
+	$('#login').css('display', 'none')
+	$('#admin').css('display', 'none')
+	$('#user').css('display', 'none')
 }
 
 function evaluateLogin () {
 	// TODO Evaluate login
-	goTo("user")
+	goTo('user')
 }
 
 // get a product node for insert in DOM
-function obtenerNodoProducto(product) {
-  var card = document.createElement("div");
-  card.classList.add("card");
-  card.classList.add("cardProducto");
+function getProductNode (product) {
+  return $('<div class="card mb-3"></div>').append(
+    $('<img src="'+product.img+'" class="card-img-top">'),
+    $('<div class="card-body"></div>').append(
+      $('<h5 class="card-title"></h5>').text(product.name),
+      $('<h5 class="card-title"></h5>').text("$"+product.value),
+      $('<a href="#" class="btn btn-primary">Comprar</a>')
+    ),
+  )
+}
 
-  var imagen = document.createElement("img");
-  imagen.src = product.img;
-  imagen.classList.add("imgProducto");
-  card.appendChild(imagen);
+function fillProductGallery () {
+  getProducts().forEach(product => {
+    let productNode = 
+    $('#products').append(
+      $('<div class="col-6 col-md-4 col-lg-3"></div>')
+        .append(getProductNode(product))
+    )
+  })
+}
 
-  var cardBody = document.createElement("div");
-  cardBody.classList.add("card-body");
-    
-  var nombre = document.createElement("h5");
-  nombre.appendChild(document.createTextNode(product.nombre));
-  cardBody.appendChild(nombre);
-
-  var valor = document.createElement("p");
-  valor.appendChild(document.createTextNode(product.valor));
-  cardBody.appendChild(valor);
-
-  var disponibilidad = document.createElement("p");
-  disponibilidad.appendChild(document.createTextNode("Disponibilidad"));
-  cardBody.appendChild(disponibilidad);
-  
-  var boton = document.createElement("a");
-  boton.classList.add("btn");
-  boton.classList.add("btnCompra");
-
-  var icon = document.createElement("i");
-  icon.classList.add("fas");
-  icon.classList.add("fa-cart-plus");
-  icon.classList.add("fa-lg");
-  icon.classList.add("iconoCarrito");
-  boton.appendChild(icon);
-
-  var btnText = document.createElement("p");
-  btnText.classList.add("iconoCarrito");
-  btnText.appendChild(document.createTextNode("Agregar al carro"));
-  
-  boton.appendChild(btnText);
-  
-  cardBody.appendChild(boton);
-  card.appendChild(cardBody);
-
-  return card;
+function getProducts () {
+  return [{
+    'name': 'test',
+    'img': 'https://www.adslzone.net/app/uploads/2019/04/borrar-fondo-imagen.jpg',
+    'value': '10000'
+  },{
+    'name': 'test',
+    'img': 'https://www.adslzone.net/app/uploads/2019/04/borrar-fondo-imagen.jpg',
+    'value': '10000'
+  },{
+    'name': 'test',
+    'img': 'https://www.adslzone.net/app/uploads/2019/04/borrar-fondo-imagen.jpg',
+    'value': '10000'
+  },{
+    'name': 'test',
+    'img': 'https://www.adslzone.net/app/uploads/2019/04/borrar-fondo-imagen.jpg',
+    'value': '10000'
+  },{
+    'name': 'test',
+    'img': 'https://www.adslzone.net/app/uploads/2019/04/borrar-fondo-imagen.jpg',
+    'value': '10000'
+  },{
+    'name': 'test',
+    'img': 'https://www.adslzone.net/app/uploads/2019/04/borrar-fondo-imagen.jpg',
+    'value': '10000'
+  },{
+    'name': 'test',
+    'img': 'https://www.adslzone.net/app/uploads/2019/04/borrar-fondo-imagen.jpg',
+    'value': '10000'
+  },{
+    'name': 'test',
+    'img': 'https://www.adslzone.net/app/uploads/2019/04/borrar-fondo-imagen.jpg',
+    'value': '10000'
+  },{
+    'name': 'test',
+    'img': 'https://www.adslzone.net/app/uploads/2019/04/borrar-fondo-imagen.jpg',
+    'value': '10000'
+  },{
+    'name': 'test',
+    'img': 'https://www.adslzone.net/app/uploads/2019/04/borrar-fondo-imagen.jpg',
+    'value': '10000'
+  }]
 }
