4bf03bfa0749d96cf0034f6f0654ab381dd646d0
Uller-U <54084699+Uller-U@users.noreply.github.com>
2020-06-29 23:10:12 -0400

    fix issue on delete product from cart

--- public/js/user/cart.js
+++ public/js/user/cart.js
@@ -7,21 +7,29 @@ let getCart = () => {return _cart}
 let addCartNode = product => $('#cart').append(getCartNode(product))
 
 // remove an element from user cart
-function removeProduct (node, id) {
+function removeProduct (node,name) {
 	$(node).parent().parent().parent().remove()
 	for (let i=0; i<_cart.length; i++)
-		_cart[i].id == id && _cart.splice(i, 1)
+		_cart[i].name.trim().toUpperCase() == name.trim().toUpperCase() && _cart.splice(i, 1)
 }
 
 // add product to cart array
 function addProductToCart (id) {
 	// check if product exist
 	let flag = false
-	_cart.forEach(product => product.id == id && (flag = true))
-	if (flag) return
-	let product = getProduct(id)
-	_cart.push(product)
-	addCartNode(product)
+	let currentProduct = getProduct(id) 
+	for(var i=0; i <_cart.length; i++){
+		let compareProduct = _cart[i]
+		if(currentProduct.name == compareProduct.name){
+			compareProduct.count ++
+			_cart[i] = compareProduct
+			return
+		}
+	}
+	currentProduct.count = 1
+	_cart.push(currentProduct)
+	addCartNode(currentProduct)
+	return
 }
 
 // add cart to orders list
@@ -43,8 +51,8 @@ function getCartNode (product) {
 			$('<div class="col-4"></div>').text(product.name),
 			$('<div class="col-3"></div>').text('$'+product.value),
 			$('<div class="col"></div>').append(
-				$('<input value="1" class="cartNodeCount">')),
+				$('<input value="'+product.count+'" class="cartNodeCount">')),
 			$('<div class="col text-right"></div>').append(
-				$('<button class="btn btn-danger" onclick="removeProduct(this, '+product.id+')"></button>').append(
+				$('<button class="btn btn-danger" onclick="removeProduct(this,\''+product.name+'\')"></button>').append(
 					$('<i class="fas fa-trash-alt"></i>')))))
 }
